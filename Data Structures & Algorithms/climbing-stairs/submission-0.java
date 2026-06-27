2c10dd1a362a30b19b24807460a7710284f67978
Dev Faldu <60572551+dev-dotcom@users.noreply.github.com>
2026-06-27 10:01:04 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int prev = 1;
+        int prev2 = 1;
+
+        for(int i = 0; i < n- 1; i ++){
+            int cur = prev + prev2;
+            prev2 = prev;
+            prev = cur;
+
+        }
+        return prev;
+        
+    }
+}
